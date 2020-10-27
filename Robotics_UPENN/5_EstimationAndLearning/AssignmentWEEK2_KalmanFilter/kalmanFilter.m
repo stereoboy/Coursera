dc66b59aa7cf02b8650d8b90bd011e37d749ef40
Jaeyoung Lee <jylee0121@gmail.com>
2020-10-27 20:40:37 +0900

    update: done Robotics Estmation WEEK2 HW
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- Robotics_UPENN/5_EstimationAndLearning/AssignmentWEEK2_KalmanFilter/kalmanFilter.m
+++ Robotics_UPENN/5_EstimationAndLearning/AssignmentWEEK2_KalmanFilter/kalmanFilter.m
@@ -5,6 +5,11 @@ function [ predictx, predicty, state, param ] = kalmanFilter( t, x, y, state, pa
     %% Place parameters like covarainces, etc. here:
     % P = eye(4)
     % R = eye(2)
+    Sigma_m = 1e-1*eye(4);
+    Sigma_o = 1e-2*eye(2);
+    C = [ 1.0, 0.0, 0.0, 0.0;
+          0.0, 1.0, 0.0, 0.0];
+    
 
     % Check if the first time running this function
     if previous_t<0
@@ -18,11 +23,40 @@ function [ predictx, predicty, state, param ] = kalmanFilter( t, x, y, state, pa
     %% TODO: Add Kalman filter updates
     % As an example, here is a Naive estimate without a Kalman filter
     % You should replace this code
-    vx = (x - state(1)) / (t - previous_t);
-    vy = (y - state(2)) / (t - previous_t);
+    %vx = (x - state(1)) / (t - previous_t);
+    %vy = (y - state(2)) / (t - previous_t);
+    dt = t - previous_t;
+    A = [  1.0, 0.0, dt, 0.0;
+           0.0, 1.0, 0.0 dt;
+           0.0, 0.0, 1.0, 0.0;
+           0.0, 0.0, 0.0, 1.0];
+    
+    P = A*param.P*transpose(A) + Sigma_m;
+    R = Sigma_o;
+    K = P*transpose(C)*inv(R + C*P*transpose(C));
+    
+    z = [x;y];
+    % update
+    param.P = (eye(4) - K*C)*param.P;
+    
+    % predict
+    
+    state_vector = transpose(state);
+    state_vector = A*state_vector + K*(z - C*A*state_vector);
+    
     % Predict 330ms into the future
-    predictx = x + vx * 0.330;
-    predicty = y + vy * 0.330;
+    % predictx = x + vx * 0.330;
+    % predicty = y + vy * 0.330;    
+    predict_A = [   1.0, 0.0, 0.330, 0.0;
+                    0.0, 1.0, 0.0 0.330;
+                    0.0, 0.0, 1.0, 0.0;
+                    0.0, 0.0, 0.0, 1.0];
+    
+    predict = predict_A*state_vector;
+
+    predictx = predict(1,1);
+    predicty = predict(2,1);
     % State is a four dimensional element
-    state = [x, y, vx, vy];
+    % state = [x, y, vx, vy];
+    state = transpose(state_vector);
 end
